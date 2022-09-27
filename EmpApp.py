b924c5631b8bb6f4c5cf1dd4f27ce98d6a84eb52
YapHongKiat <72433605+YapHongKiat@users.noreply.github.com>
2022-09-28 03:19:26 +0800

    get s3 image

--- EmpApp.py
+++ EmpApp.py
@@ -105,7 +105,7 @@ def aboutUs():
     cursor.execute('SELECT * FROM employee')
     init_employees = cursor.fetchall()
     emp_image_file = show_image(custombucket)
-    test_list = list(0, 1, 2, 3)
+    test_list = (0, 1, 2, 3)
     employees = init_employees + (test_list,)
     #emp_image_file = 'https://pbs.twimg.com/profile_images/1389140738827501568/RUeCH5Dg_400x400.jpg'
     return render_template(
