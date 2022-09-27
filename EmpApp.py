6459be2b3c9faa4d7f436ab2bf0f934550b2500d
YapHongKiat <72433605+YapHongKiat@users.noreply.github.com>
2022-09-27 22:58:42 +0800

    get db

--- EmpApp.py
+++ EmpApp.py
@@ -87,10 +87,8 @@ def AddEmp():
 @app.route('/aboutUs')
 def aboutUs():
     cursor = db_conn.cursor()
-    cursor.execute('SELECT * FROM employee')
+    cursor.execute('SELECT * FROM employee WHERE emp_id=%s', ('1'))
     db = cursor.fetchone()
-    print(db)
-    """
     emp_id = db['emp_id']
     first_name = db['first_name']
     last_name = db['last_name']
@@ -102,7 +100,6 @@ def aboutUs():
         emp_image_file='https://pbs.twimg.com/profile_images/1389140738827501568/RUeCH5Dg_400x400.jpg',
         pri_skill='cloud', location='Sepang'
     )
-    """
 
 
 if __name__ == '__main__':
