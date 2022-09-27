b7a7e132f5a6021f36da827b998c550e154ae7eb
YapHongKiat <72433605+YapHongKiat@users.noreply.github.com>
2022-09-28 02:23:39 +0800

    get s3 image

--- EmpApp.py
+++ EmpApp.py
@@ -23,14 +23,14 @@ table = 'employee'
 
 def show_image(bucket):
     s3_client = boto3.client('s3')
-    public_urls = tuple()
+    public_urls = []
     try:
         for item in s3_client.list_objects(Bucket=bucket)['Contents']:
             presigned_url = s3_client.generate_presigned_url(
                 'get_object', Params={'Bucket': bucket, 'Key': item['Key']}, ExpiresIn=100)
             public_urls.append(presigned_url)
     except Exception as e:
-        public_urls += ('fail{}', item)
+        public_urls.append('fail{}', item)
         # pass
     # print("[INFO] : The contents inside show_image = ", public_urls)
     return public_urls
