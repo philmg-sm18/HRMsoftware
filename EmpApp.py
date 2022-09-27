eb383606febbe8af3504bb21b120c0187ca20aa6
YapHongKiat <72433605+YapHongKiat@users.noreply.github.com>
2022-09-27 23:06:58 +0800

    get db

--- EmpApp.py
+++ EmpApp.py
@@ -94,7 +94,7 @@ def aboutUs():
     last_name = db[2]
     pri_skill = db[3]
     location = db[4]
-    emp_image_file = db[5]
+    #emp_image_file = "read from S3"
     return render_template(
         'AboutUs.html', fname=first_name, lname=last_name,
         emp_image_file='https://pbs.twimg.com/profile_images/1389140738827501568/RUeCH5Dg_400x400.jpg',
